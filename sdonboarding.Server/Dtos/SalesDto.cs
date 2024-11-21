88836da1233f3e6853323c77b6b2b575b24c695b
pasindu <pasindujalath@gmail.com>
2024-11-22 10:04:29 +1300

    implement Store Rest end points

---
+++ sdonboarding.Server/Dtos/SalesDto.cs
@@ -0,0 +1,16 @@
+﻿namespace sdonboarding.Server.Dtos
+{
+    public class SalesDto
+    {
+        public int Id { get; set; }
+
+        public int? CustomerId { get; set; }
+
+        public int? ProductId { get; set; }
+
+        public int? StoreId { get; set; }
+
+        public DateTime? DateSold { get; set; }
+
+    }
+}
